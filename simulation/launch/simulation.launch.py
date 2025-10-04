79eab862a6938bf9d7b4b1ab3aecc884fa319ebc
Karolina Kozubik <k4aro0@gmail.com>
2025-10-04 19:15:58 +0200

    feat: spawn the drone

--- simulation/launch/simulation.launch.py
+++ simulation/launch/simulation.launch.py
@@ -1,19 +1,43 @@
-# from pathlib import Path
+import os
+from pathlib import Path
 from ament_index_python import get_package_share_path
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch_ros.actions import SetParameter
 
 
 def generate_launch_description():
     simulation_share = get_package_share_path("simulation")
+    px4_path = Path(os.environ.get("PX4_PATH"))
+    px4_bin = px4_path / "build" / "px4_sitl_default" / "bin" / "px4"
+
+    set_resources = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH",
+        (px4_path / "Tools" / "simulation" / "gz" / "models").as_posix(),
+    )
+
+    set_sim_time = SetParameter("use_sim_time", value=True)
 
     run_gazebo_sim = ExecuteProcess(
         cmd=["gz", "sim", "-r", (simulation_share / "worlds" / "basic.sdf").as_posix()],
         output="screen",
     )
 
+    run_autopilot = ExecuteProcess(
+        cmd=[px4_bin.as_posix()],
+        additional_env={
+            "PX4_SYS_AUTOSTART": "4001",
+            "PX4_SIM_MODEL": "x500",
+            "PX4_GZ_MODEL_POSE": "0 0 0.17696 0 0 0",
+        },
+        output="screen",
+    )
+
     ld = LaunchDescription()
 
+    ld.add_action(set_resources)
+    ld.add_action(set_sim_time)
     ld.add_action(run_gazebo_sim)
+    ld.add_action(run_autopilot)
 
     return ld
