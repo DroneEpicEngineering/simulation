b1082a87255c7cb3aecd1898c96501a3f97a07dc
Karolina Kozubik <k4aro0@gmail.com>
2025-10-04 18:19:52 +0200

    feat: add world

--- simulation/launch/simulation.launch.py
+++ simulation/launch/simulation.launch.py
@@ -1,5 +1,19 @@
+# from pathlib import Path
+from ament_index_python import get_package_share_path
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
 
 def generate_launch_description():
+    simulation_share = get_package_share_path("simulation")
+
+    run_gazebo_sim = ExecuteProcess(
+        cmd=["gz", "sim", "-r", (simulation_share / "worlds" / "basic.sdf").as_posix()],
+        output="screen",
+    )
+
     ld = LaunchDescription()
+
+    ld.add_action(run_gazebo_sim)
+
     return ld
